0e7665111496a29472f14ce36c33d6b57a7d0974
Rohit Pandey <132219458+pandey8369@users.noreply.github.com>
2025-03-24 01:33:54 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -94,7 +94,7 @@ function NavBar() {
 
             <Nav.Item className="fork-btn">
               <Button
-                href="https://github.com/soumyajit4419/Portfolio"
+                href="https://github.com/pandey8369/Portfolio"
                 target="_blank"
                 className="fork-btn-inner"
               >
